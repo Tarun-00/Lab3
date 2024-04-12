33d18803f05c3afc70c4af63ddb3a812e89f16c6
Tarun Gahlot <69211877+Tarun-00@users.noreply.github.com>
2024-04-12 00:24:18 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+//alert("Hello World");
+
+//Activity 2
+
+let personName = "Robin";
+let personAge = "19";
+let personiskid = true;
+let knowsJavaScript = false;
+let hobbies; // Robin's hobbies are not defined yet
+let previousEmployer = null; // Robin is student
+
+console.log("A random person name is: " + personName);
+
+// Activity 3
+let todolist = ["tax return", "go to embassy", "change nail"];
+
+let myBackpack = {
+  brand: "Arctic Fox",
+  color: "black",
+  isUsed: true,
+  numberOfUsed: 20,
+};
+
+console.log("my first to do is: " + todolist[0]);
+console.log("my backpack color is: " + myBackpack.color);
